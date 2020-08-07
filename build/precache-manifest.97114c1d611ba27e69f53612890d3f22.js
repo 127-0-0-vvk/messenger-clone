6b98ea76a2bdc87b19f04c2457caf100a5527d31
Vivek <41470324+127-0-0-vvk@users.noreply.github.com>
2020-08-08 00:51:15 +0100

    Add files via upload

---
+++ build/precache-manifest.97114c1d611ba27e69f53612890d3f22.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04e4463e7b5fc3ccb12c4bfef4c806d5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "865e285f52d0f168affd",
+    "url": "/static/css/main.202c4168.chunk.css"
+  },
+  {
+    "revision": "a7269d34a9769a3e43e2",
+    "url": "/static/js/2.ca47557a.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/static/js/2.ca47557a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "865e285f52d0f168affd",
+    "url": "/static/js/main.9540cec2.chunk.js"
+  },
+  {
+    "revision": "83b7cc9f86aaf60b17f5",
+    "url": "/static/js/runtime-main.9076f898.js"
+  }
+]);
